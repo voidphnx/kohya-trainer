9619d5842786da6d282338b2404a47eaae32a544
voidphnx <143304428+voidphnx@users.noreply.github.com>
2025-01-29 14:39:01 +0300

    Update config_util.py

--- library/config_util.py
+++ library/config_util.py
@@ -53,7 +53,7 @@ class BaseSubsetParams:
   shuffle_caption: bool = False
   caption_separator: str = (",",)
   keep_tokens: int = 0
-  keep_tokens_separator: str = ("|||",)
+  keep_tokens_separator: str = ("^",)
   secondary_separator: Optional[str] = (";",)
   enable_wildcard: bool = False
   color_aug: bool = False
